86efda0bb51aa2b046d6506d8df8e7c1edeec20f
Afonso Pires <107490044+afonsopires1@users.noreply.github.com>
2023-10-15 17:21:43 +0100

    Update useTodo.ts

--- lib/frontend/hooks/useTodo.ts
+++ lib/frontend/hooks/useTodo.ts
@@ -1,15 +1,43 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-export function useTodo(initialItems?: string[]) {
-  const [todos, setTodos] = useState<string[]>(initialItems || []);
+//structure of each to-do item
+interface Item {
+  id: number;
+  text: string;
+  completed: boolean;
+}
 
-  /* Add your todo methods here */
-  const addItem = () => {
-    setTodos((prevTodos) => [...prevTodos, "New todo"]);
+export function useTodo() {
+
+  //initialize to-do examples
+  const [todos, setTodos] = useState<Item[]>([
+    { id: 1, text: 'Teste', completed: false },
+    { id: 2, text: 'Teste2', completed: false },
+  ]);
+
+  const [input, setInput] = useState<string>('');
+
+
+  //function to delete a to-do by ID
+  const deleteTodo = (id: number) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
   };
+
+   // Function to handle the button click event for adding a new to/do
+  const addTodo = (text: string) => {
+    if (text.trim() !== '') {
+      const newTodo: Item = { id: Date.now(), text, completed: false };
+      setTodos((prevTodos) => [...prevTodos, newTodo]);
+    }
+  };
+
+  
 
   return {
     todos,
-    addTodo: addItem,
+    input,
+    setInput,
+    deleteTodo,
+    addTodo,
   };
 }
